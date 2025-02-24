2e45d637003fa44a3ea8590bfa29cdd91f67f6a2
AlexSanches1 <sanches2026@gmail.com>
2025-02-24 10:04:13 -0500

    Test homebrew WIP

--- Formula/asimov-cli.rb
+++ Formula/asimov-cli.rb
@@ -3,41 +3,45 @@ class AsimovCli < Formula
   homepage "https://github.com/asimov-platform/asimov-cli"
   version "25.0.0-dev.2"
 
-  # --- macOS (Intel & ARM) ---
   on_macos do
     if Hardware::CPU.arm?
+      # macOS ARM
       url "https://github.com/asimov-platform/asimov-cli/releases/download/#{version}/asimov-macos-arm.gz"
       sha256 "af05128473c3dfda48e7740d2a456a7996a6391ac46a3ef0620cb803afaeb793"
     else
+      # macOS Intel
       url "https://github.com/asimov-platform/asimov-cli/releases/download/#{version}/asimov-macos-x86.gz"
       sha256 "332f2a586bb3d702db60b165f09b96a01995034a35ab109713d01ba53f0cb2cf"
     end
   end
 
-  # --- Linux (Intel & ARM) ---
   on_linux do
     if Hardware::CPU.arm?
+      # Linux ARM
       url "https://github.com/asimov-platform/asimov-cli/releases/download/#{version}/asimov-linux-arm.gz"
       sha256 "e9ca2393fe6147a01f10a36bd162b8f014fcfc1e964bb65a33e6d0920b34960d"
     else
+      # Linux Intel
       url "https://github.com/asimov-platform/asimov-cli/releases/download/#{version}/asimov-linux-x86.gz"
       sha256 "a64251f846d0cc0280a7f83f25f0db0c4f15ef4caf349ccd45dad22250866060"
     end
   end
 
   def install
-    # Decompress the .gz file we downloaded
+    # Decompress the downloaded .gz file first
     system "gunzip", cached_download
 
-    # After decompression, we should have something like:
-    # asimov-macos-x86  or asimov-linux-arm  etc.
-    # We install/rename it to "asimov" in bin/
-    bin.install Dir["asimov-*"][0] => "asimov"
+    # The uncompressed file has the same name minus ".gz"
+    uncompressed = Pathname(cached_download.to_s.sub(/\.gz$/, ""))
+
+    # Install it as "asimov" in Homebrew's bin directory
+    bin.install uncompressed => "asimov"
   end
 
   test do
-    # Check that the CLI runs
+    # Basic check that the CLI runs
     system "#{bin}/asimov", "--version"
   end
 end
+
 
