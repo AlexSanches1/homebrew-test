8a9b9322e53fde82c76a8a5e5a03ef47071c0c6b
AlexSanches1 <sanches2026@gmail.com>
2025-02-24 09:53:13 -0500

    Test homebrew WIP

--- Formula/asimov-cli.rb
+++ Formula/asimov-cli.rb
@@ -7,10 +7,10 @@ class AsimovCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/asimov-platform/asimov-cli/releases/download/#{version}/asimov-macos-arm.gz"
-      sha256 "<af05128473c3dfda48e7740d2a456a7996a6391ac46a3ef0620cb803afaeb793>"
+      sha256 "af05128473c3dfda48e7740d2a456a7996a6391ac46a3ef0620cb803afaeb793"
     else
       url "https://github.com/asimov-platform/asimov-cli/releases/download/#{version}/asimov-macos-x86.gz"
-      sha256 "<332f2a586bb3d702db60b165f09b96a01995034a35ab109713d01ba53f0cb2cf>"
+      sha256 "332f2a586bb3d702db60b165f09b96a01995034a35ab109713d01ba53f0cb2cf"
     end
   end
 
@@ -18,10 +18,10 @@ class AsimovCli < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/asimov-platform/asimov-cli/releases/download/#{version}/asimov-linux-arm.gz"
-      sha256 "<e9ca2393fe6147a01f10a36bd162b8f014fcfc1e964bb65a33e6d0920b34960d>"
+      sha256 "e9ca2393fe6147a01f10a36bd162b8f014fcfc1e964bb65a33e6d0920b34960d"
     else
       url "https://github.com/asimov-platform/asimov-cli/releases/download/#{version}/asimov-linux-x86.gz"
-      sha256 "<a64251f846d0cc0280a7f83f25f0db0c4f15ef4caf349ccd45dad22250866060>"
+      sha256 "a64251f846d0cc0280a7f83f25f0db0c4f15ef4caf349ccd45dad22250866060"
     end
   end
 
